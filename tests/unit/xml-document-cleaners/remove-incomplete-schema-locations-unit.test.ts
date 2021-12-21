de1e289e906a6635e8b8244a9e27adfc44c84e45
Fernando Isidro L <luffynando@gmail.com>
2021-12-21 01:28:31 -0600

    fix: name same equals on same file

---
+++ tests/unit/xml-document-cleaners/remove-incomplete-schema-locations-unit.test.ts
@@ -0,0 +1,43 @@
+import { RemoveIncompleteSchemaLocations } from '../../../src/xml-document-cleaners/remove-incomplete-schema-locations';
+
+describe('RemoveIncompleteSchemaLocations', () => {
+    let cleaner: RemoveIncompleteSchemaLocations;
+
+    beforeAll(() => {
+        cleaner = new RemoveIncompleteSchemaLocations();
+    });
+
+    test('schema location value namespace xsd pair to array', () => {
+        const input = [
+            'http://tempuri.org/root http://tempuri.org/root.xsd',
+            'http://tempuri.org/foo',
+            'http://tempuri.org/bar http://tempuri.org/bar.xsd',
+            'http://tempuri.org/one.xsd',
+            'http://tempuri.org/two http://tempuri.org/two.xsd',
+            'http://tempuri.org/three http://tempuri.org/three',
+        ]
+            .join(' ')
+            .trim();
+
+        const expectedPairs = {
+            'http://tempuri.org/root': 'http://tempuri.org/root.xsd',
+            'http://tempuri.org/bar': 'http://tempuri.org/bar.xsd',
+            'http://tempuri.org/two': 'http://tempuri.org/two.xsd',
+        };
+
+        const pairs = cleaner.schemaLocationValueNamespaceXsdPairToArray(input);
+        expect(pairs).toStrictEqual(expectedPairs);
+    });
+
+    test.each([
+        ['location', false],
+        ['locationxsd', false],
+        ['', false],
+        ['location.xsd', true],
+        ['location.XSD', true],
+        ['location.Xsd', true],
+        ['location..xsd', true],
+    ])('uri ends with xsd', (uri: string, expected: boolean) => {
+        expect(cleaner.uriEndsWithXsd(uri)).toBe(expected);
+    });
+});
