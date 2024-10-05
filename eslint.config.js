6cd7367e716c952864f4daa9d5b03ac1813c85f7
Fernando Isidro Luna <luffynando@gmail.com>
2024-10-05 00:17:05 -0600

    chore: :arrow_up: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,4 @@ import nodecfdiConfig from '@nodecfdi/eslint-config';
 
 const { defineConfig } = nodecfdiConfig(import.meta.dirname, { vitest: true });
 
-export default defineConfig({
-  rules: {
-    'import-x/no-named-as-default-member': 'off',
-  },
-});
+export default defineConfig();
